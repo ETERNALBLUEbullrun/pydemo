d07d2b9e82e763cef51fcf317beff77a05a94bd3
test <localhost>
2021-11-21 19:42:07 -0800

    +Test.py:Exercises various Python features

---
+++ Test.py
@@ -0,0 +1,22 @@
+#Test.py exercises various Python features
+x = 1
+xRef = x
+x <<= 8
+xVal = int(str(x))
+y = int("256")
+b = "2"
+c = 1
+d = """"double" and 'single' quotes"""
+e = "'single' quotes"
+f = '"double"'
+if d != f + " and " + e:
+	print("E: string escaping fail")
+if x is not x or x is xRef or x is not xVal:
+	print("E: referencing fail")
+if x + int(b) != 258:
+	print("E: int/str conversion fail")
+if y in ["256"]:
+    print("E: 'in' should compare address, not value")
+if 1 != xRef:
+    print("E: reference should be to original value only")
+
